41d30226438cdeced06490c8fc140f77cc9aa52a
Abdifatah.206 <shire2567@gmail.com>
2019-07-28 11:48:37 -0700

    placeholder added input fields

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,8 @@
+function calc(){
+  var n1 = document.getElementById('n1').value;
+  var n2 = document.getElementById('n2').value;
+  var n3 = document.getElementById('n3').value;
+  var result = document.getElementById('result').value = n1*n2*n3;
+
+
+}
